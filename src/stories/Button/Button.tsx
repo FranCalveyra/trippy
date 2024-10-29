cf78fecb5cf4c9f046da98120cc040c88af732d7
Tomás <troldanborjas@mail.austral.edu.ar>
2024-10-29 19:18:27 -0300

    Added Button from example (will be adapted to ours); set up cva

--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, {ReactNode,ButtonHTMLAttributes} from 'react'
+import {cva, VariantProps} from "class-variance-authority";
 
-import './button.css';
 
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
-  /** Button contents */
-  label: string;
-  /** Optional click handler */
-  onClick?: () => void;
+const buttonVariant = cva(['w-full sm:w-fit','rounded-lg '],{
+    variants:{
+        variant:{
+            fulfilled:[
+                'bg-primary-500 text-white',
+                'hover:bg-primary-700 hover:text-white',
+                'disabled:bg-grey-300 disabled:text-grey-700',
+            ],
+            outlined:[
+                'bg-white text-primary-500 border border-primary-500',
+                'hover:bg-primary-300',
+                'disabled:bg-grey-300 disabled:border-grey-300 disabled:text-grey-700',
+            ],
+            ghost: [
+                "bg-transparent text-primary-500",
+                "hover:bg-primary-300 hover:text-primary-500",
+                "disabled:bg-grey-100 disabled:text-grey-700"
+            ],
+            white: [
+                "bg-white text-primary-500",
+                "hover:bg-primary-300 hover:text-primary-500",
+                "disabled:bg-grey-100 disabled:text-grey-700"
+            ],
+            black:[]
+        },
+        size:{
+            small:['px-4 py-2 text-xs'],
+            medium:['px-4 py-3 text-sm'],
+            large:['px-4 py-3 text-base'],
+        }
+    },
+    defaultVariants:{
+        variant:'fulfilled',
+        size:'medium'
+    }
+})
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariant>{
+
 }
 
-/** Primary UI component for user interaction */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+const Button = ({variant,size, ...rest}:ButtonProps) => {
+    return <button className={buttonVariant({size, variant})} {...rest}/>
+}
+
+export default Button
--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from './Button/Button.tsx';
+import { ButtonExample } from './ButtonExample/ButtonExample.tsx';
 import './header.css';
 
 type User = {
@@ -42,12 +42,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <ButtonExample size="small" onClick={onLogout} label="Log out" />
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <ButtonExample size="small" onClick={onLogin} label="Log in" />
+            <ButtonExample primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
       </div>
