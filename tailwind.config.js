d13affb43eb2622f44c9dab1e2c93723b33c4387
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-10-29 20:55:37 -0300

    added status and neutral colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,24 @@ export default {
         500: '#019296',
         600: '#018387',
         700: '#003A3C',
+      },
+      status:{
+        success: '#83C45B',
+        warning: '#FCEA49',
+        error: '#FF5146'
+      },
+      neutral:{
+        black: '#000000',
+        900: '#212529',
+        800: '#343A40',
+        700: '#495057',
+        600: '#6C757D',
+        500: '#ADB5BD',
+        400: '#CED4DA',
+        300: '#DEE2E6',
+        200: '#E9ECEF',
+        100: '#F8F9FA',
+        white: '#FFFFFF'
       }
     },
     fontSize:{
