454f89ce80692436a10760e66563082706b78783
agussoul <aalma@mail.austral.edu.ar>
2024-11-06 18:33:29 -0300

    xue hue

--- src/stories/searchBar/SearchBar.tsx
+++ src/stories/searchBar/SearchBar.tsx
@@ -2,20 +2,11 @@
 interface SearchBarProps {
     placeholder: string;
     imageUrl: string;
-    onClick: () => void;
 }
 
-export const SearchBar = ({ placeholder, imageUrl, onClick }: SearchBarProps) => {
+export const SearchBar = ({ placeholder, imageUrl}: SearchBarProps) => {
     return (
         <div className="w-[365px] h-[78px] bg-primary-200 rounded-modal flex items-center px-4 gap-3">
-            <button onClick={onClick}
-                    className="flex items-center justify-center w-[20px] h-[20px] text-neutral-black">
-                <div className="flex flex-col space-y-1">
-                    <div className="w-[16px] h-[2px] bg-neutral-black"></div>
-                    <div className="w-[16px] h-[2px] bg-neutral-black"></div>
-                    <div className="w-[16px] h-[2px] bg-neutral-black"></div>
-                </div>
-            </button>
 
             <input
                 type="text"
