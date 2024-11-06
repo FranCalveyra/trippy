33a7a89074ff1b793f9cf1220ec388e1abc21a7a
= <aalma@mail.austral.edu.ar>
2024-11-06 16:50:53 -0300

    elfinfolin

--- src/stories/ContactCard/ContactCard.tsx
+++ src/stories/ContactCard/ContactCard.tsx
@@ -7,13 +7,11 @@ interface ContactCardProps {
 
 export const ContactCard = ({ name, message, imageUrl }: ContactCardProps) => {
     return (
-        <div className="w-[365px] h-[77.69px] relative">
-            <div className="w-[365px] h-[78px] px-[3px] left-0 top-0 absolute bg-blue-100 rounded-[20px] flex items-center gap-2.5">
-                <img className="w-[51.68px] h-[51.69px] rounded-full" src={imageUrl} alt={`${name}'s avatar`} />
-                <div className="flex flex-col">
-                    <div className="text-gray-900 text-base font-bold">{name}</div>
-                    <div className="text-gray-700 text-base font-normal">{message}</div>
-                </div>
+        <div className="w-[365px] h-[78px] relative bg-primary-200 rounded-modal flex items-center gap-3 p-3">
+            <img className="w-[52px] h-[52px] rounded-full" src={imageUrl} alt={`${name}'s avatar`} />
+            <div className="flex flex-col">
+                <div className="text-neutral-black font-bold text-body">{name}</div>
+                <div className="text-neutral-700 font-normal text-body">{message}</div>
             </div>
         </div>
     );
