4b6451f329f0649dbad006391433c2414de60778
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-06 19:32:30 -0300

    betta warning

--- src/stories/notification/Notification.tsx
+++ src/stories/notification/Notification.tsx
@@ -2,13 +2,12 @@ import Icon, { IconProps } from "../Icon/Icon.tsx";
 import { cva } from "class-variance-authority";
 
 const notificationVariant = cva(
-    "w-full max-w-[393px] h-auto flex items-center px-4 gap-3",
+    "w-full max-w-[393px] h-auto px-4 py-3 gap-3",
     {
         variants: {
             variant: {
-                default: "border-b border-primary-300",
-                extended: "w-full max-w-[600px] border border-primary-300 rounded-[25px] p-6 flex-col items-start gap-4",
-                warning: "w-full max-w-[319px] h-auto bg-status-warning rounded-[13px]"
+                default: "border-b border-primary-300 flex items-center",
+                warning: "w-full max-w-[600px] bg-status-warning rounded-[13px] p-6 flex flex-col gap-4"
             },
         },
         defaultVariants: {
@@ -27,15 +26,31 @@ interface NotificationProps {
 export const Notification = ({ variant, iconVariant, title, description }: NotificationProps) => {
     return (
         <div className={notificationVariant({ variant })}>
-            <Icon variant={iconVariant} />
-            <div className="flex flex-col w-full">
-                    <div className="text-neutral-black font-bold font-['Albert Sans'] leading-normal tracking-tight text-sm md:text-base lg:text-lg">
-                        {title}
+            {variant === "warning" ? (
+                <div className="flex items-start gap-4 w-full">
+                    <Icon variant={iconVariant} />
+                    <div className="flex flex-col">
+                        <div className="text-neutral-black font-bold font-['Albert Sans'] leading-normal tracking-tight text-sm md:text-base lg:text-lg">
+                            {title}
+                        </div>
+                        <div className="text-neutral-black font-normal font-['Albert Sans'] leading-normal tracking-tight text-sm md:text-base lg:text-lg break-words">
+                            {description}
+                        </div>
                     </div>
-                <div className="text-neutral-black font-normal font-['Albert Sans'] leading-normal tracking-tight text-sm md:text-base lg:text-lg break-words">
-                    {description}
                 </div>
-            </div>
+            ) : (
+                <>
+                    <Icon variant={iconVariant} />
+                    <div className="flex flex-col w-full">
+                        <div className="text-neutral-black font-bold font-['Albert Sans'] leading-normal tracking-tight text-sm md:text-base lg:text-lg">
+                            {title}
+                        </div>
+                        <div className="text-neutral-black font-normal font-['Albert Sans'] leading-normal tracking-tight text-sm md:text-base lg:text-lg break-words">
+                            {description}
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
