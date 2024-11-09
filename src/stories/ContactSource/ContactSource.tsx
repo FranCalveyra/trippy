cf341bb016894cd5837eed3ef0d1112a3d3b5be7
FranCalveyra <franciscocalveyra24@gmail.com>
2024-11-09 16:26:30 -0300

    Finished Contact Source component and its Modal

--- src/stories/ContactSource/ContactSource.tsx
+++ src/stories/ContactSource/ContactSource.tsx
@@ -8,7 +8,7 @@ export const ContactSource =({picturePath="https://support.discord.com/hc/user_i
         <div className="w-full h-full flex flex-col justify-center items-center">
             <img alt={""} className="w-11 h-11 rounded-lg" src={picturePath}/>
             <div
-                className="w-20 h-3 text-center text-black text-xs font-roboto font-normal leading-4 tracking-wide break-words">
+                className="w-20 h-3 text-center text-primary-700 text-xs font-roboto font-normal leading-4 tracking-wide break-words">
                 {sourceName}
             </div>
         </div>
--- src/stories/ContactSource/Modal/ContactSourceModal.tsx
+++ src/stories/ContactSource/Modal/ContactSourceModal.tsx
@@ -1,19 +1,22 @@
-import {ContactSource} from "../ContactSource.tsx";
+import { ContactSource } from "../ContactSource.tsx";
 
 interface ContactSourceModalProps {
-    sources: {picturePath: string, sourceName: string}[]
+    sources: { picturePath: string, sourceName: string }[]
 }
 
-export const ContactSourceModal = ({sources = []}: ContactSourceModalProps) => {
+export const ContactSourceModal = ({ sources = [] }: ContactSourceModalProps) => {
     return (
-        <div className="w-auto h-auto px-7 py-6 flex flex-col justify-start items-start gap-1.5 bg-[#DADDFC] rounded-t-[17.58px]">
-            <div className="grid grid-cols-4 gap-2.5 w-full">
-                {sources.length > 0 ? sources.map(s => (
-                    <div key={s.sourceName} className="w-14 h-14 flex flex-col justify-center items-center">
-                        <ContactSource picturePath={s.picturePath} sourceName={s.sourceName}/>
+        <div className="w-[376px] h-auto px-7 py-6 flex flex-col justify-start items-center gap-4 bg-[#FFFFFF] rounded-t-[17.58px] shadow-lg">
+            <div className="text-xl font-bold text-primary-700 tracking-wide text-center mb-4">
+                Select contact source
+            </div>
+            <div className="grid grid-cols-4 gap-4 w-full">
+                {sources.length > 0 ? sources.map(source => (
+                    <div key={source.sourceName} className="flex flex-col items-center">
+                        <ContactSource picturePath={source.picturePath} sourceName={source.sourceName} />
                     </div>
                 )) : null}
             </div>
         </div>
-    )
-}
+    );
+};
