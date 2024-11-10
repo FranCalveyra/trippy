6e2f243d7317d6cdc72d88bb41eef25445ba694f
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-10 00:01:16 -0300

    notification

--- src/stories/notification/Notification.tsx
+++ src/stories/notification/Notification.tsx
@@ -6,7 +6,7 @@ const notificationVariant = cva(
     {
         variants: {
             variant: {
-                default: "w-full max-w-[393px] h-auto border-b border-primary-300 items-center",
+                default: "w-full max-w-[393px] h-auto border-b border-neutral-600 items-center",
                 warning: "w-[319px] h-[134px] bg-status-warning rounded-[13px] flex flex-col justify-center px-6 gap-1"
             },
         },
@@ -27,14 +27,14 @@ export const Notification = ({ variant, iconVariant, title, description }: Notif
     return (
         <div className={notificationVariant({ variant })}>
             <div className="flex items-start gap-3 w-full">
-                <div className="flex items-center justify-center w-8 h-8 bg-[#daddfc] rounded-full">
-                    <Icon variant={iconVariant} color="s" />
+                <div className="flex items-center justify-center w-8 h-8 bg-secondary-200 rounded-full">
+                    <Icon variant={iconVariant} color="" />
                 </div>
                 <div className="flex flex-col">
-                    <div className="text-[#003a3c] font-bold text-base leading-tight font-['Albert Sans']">
+                    <div className="text-primary-700 font-bold text-base leading-tight truncate overflow-hidden whitespace-nowrap text-ellipsis">
                         {title}
                     </div>
-                    <div className="text-[#003a3c] font-normal text-sm leading-tight font-['Roboto']">
+                    <div className="text-primary-700 font-normal text-sm leading-tight truncate overflow-hidden whitespace-nowrap text-ellipsis">
                         {description}
                     </div>
                 </div>
