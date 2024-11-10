a3851fb63706887b84bb189a35929dc105fb2415
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-11-10 00:34:47 -0300

    warning not exactly same but almost

--- src/stories/notification/Notification.tsx
+++ src/stories/notification/Notification.tsx
@@ -7,7 +7,7 @@ const notificationVariant = cva(
         variants: {
             variant: {
                 default: "w-full max-w-[393px] h-auto border-b border-neutral-600 items-center",
-                warning: "w-[319px] h-[134px] pb-2.5 justify-end items-center inline-flex",
+                warning: "w-[319px] h-[134px] rounded-[13px] pb-2.5 justify-end items-center inline-flex border",
             },
         },
         defaultVariants: {
@@ -26,19 +26,18 @@ interface NotificationProps {
 export const Notification = ({ variant, iconVariant, title, description }: NotificationProps) => {
     return (
         <div className={notificationVariant({ variant })}>
-            <div className={`flex ${variant === "warning" ? "px-3 py-6 bg-white rounded-[13px] justify-start items-start gap-2 inline-flex" 
+            <div className={`flex ${variant === "warning" ? "px-3 py-6 bg-white rounded-[13px] justify-start items-start gap-2 inline-flex"
                 : "items-start gap-3 w-full"}`}>
-                <div className={`${variant === "warning" ? "w-[38.77px] h-[41.23px] relative bg-status-warning/50 rounded-full flex items-center justify-center" 
+                <div className={`${variant === "warning" ? "w-[38.77px] h-[41.23px] relative bg-status-warning/50 rounded-[99px] flex items-center justify-center"
                     : "flex items-center justify-center w-8 h-8 bg-secondary-200 rounded-full"}`}>
-
                     <Icon variant={iconVariant} color="" />
                 </div>
                 <div className="flex flex-col">
-                    <div className={`${variant === "warning" ? "self-stretch h-[22px] text-primary-700 text-base font-bold leading-normal tracking-tight truncate overflow-hidden whitespace-nowrap text-ellipsis" 
+                    <div className={`${variant === "warning" ? "self-stretch h-[22px] text-primary-700 text-base font-bold leading-normal tracking-tight truncate overflow-hidden whitespace-nowrap text-ellipsis"
                         : "max-w-[393px] text-primary-700 font-bold text-base leading-tight truncate overflow-hidden whitespace-nowrap text-ellipsis"}`}>
                         {title}
                     </div>
-                    <div className={`${variant === "warning" ? "w-[202px] text-primary-700 text-base font-normal leading-normal tracking-tight truncate overflow-hidden whitespace-nowrap text-ellipsis" 
+                    <div className={`${variant === "warning" ? "w-[202px] text-primary-700 text-base font-normal leading-normal tracking-tight overflow-hidden whitespace-normal"
                         : "max-w-[393px] text-primary-700 font-normal text-sm leading-tight truncate overflow-hidden whitespace-nowrap text-ellipsis"}`}>
                         {description}
                     </div>
