620f84080bf22e260323c4adbbd3c22cc69a20c2
FranCalveyra <franciscocalveyra24@gmail.com>
2024-11-12 20:22:52 -0300

    Border radius fix

--- src/stories/Widget/Widget.tsx
+++ src/stories/Widget/Widget.tsx
@@ -7,7 +7,7 @@ import mapPath from "../assets/Current Location.png"
 export const Widget = ({ imagePath = mapPath, widgetTitle = "Current Location" }: WidgetProps) => {
     return (
         <div className="flex justify-center items-center w-full h-full bg-white">
-            <div className="w-[261px] h-[165px] relative bg-primary-100 rounded-t-lg rounded-bl-lg">
+            <div className="w-[261px] h-[165px] relative bg-primary-100 rounded-tl-lg rounded-br-lg rounded-bl-lg">
                 <div className="w-[211.07px] h-[33px] absolute top-[15.23px] left-[24.97px] text-primary-700 text-base font-sans font-bold leading-6 tracking-wide">
                     {widgetTitle}
                 </div>
