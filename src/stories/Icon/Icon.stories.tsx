6098755d54cd8cb115ce56f68d420ba8be8bd4e0
nicolasberretta <neberretta@mail.austral.edu.ar>
2024-11-13 16:52:33 -0300

    hotfix icon stories

--- src/stories/Icon/Icon.stories.tsx
+++ src/stories/Icon/Icon.stories.tsx
@@ -1,14 +1,21 @@
 import {Meta, StoryObj} from "@storybook/react";
-import Button from "./Icon.tsx";
 import Icon from "./Icon.tsx";
+import tailwindConfig from "../../../tailwind.config";
 
 const meta: Meta<typeof Icon> ={
     title:'Atoms/Icon',
     component:Icon,
+    parameters: {
+        layout: 'centered',
+    },
     tags: ['autodocs'],
+    args:{
+        variant: 'notifications',
+        color: tailwindConfig.theme.colors.primary["700"]
+    },
     control:{
         type: 'selection'
-    }
+    },
 }
 
 export default meta
@@ -17,6 +24,9 @@ type Story = StoryObj<typeof Icon>
 
 export const Default: Story = {
     name:'Default',
-    render: () => <Icon variant={'notifications'}/>
+    args:{
+        variant: 'notifications',
+        color: tailwindConfig.theme.colors.primary["700"]
+    }
 }
 
